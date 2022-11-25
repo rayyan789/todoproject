80e7c63720369e82015c5864da249ae468892480
Balagangadhar-Dev <bkandula@keydabra.com>
2022-11-25 14:39:51 +0530

    added userauth app

--- todoapp/models.py
+++ todoapp/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 # Create your models here.
 class TodoListItem(models.Model):
+    user = models.CharField(max_length=100)
     content = models.TextField() 
--- todoproject/urls.py
+++ todoproject/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todoapp.urls',)),
+    path('', include('userauth.urls',)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
